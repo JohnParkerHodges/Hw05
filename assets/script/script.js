f5cc982cca122046f6a6a7c776306ba0f246714a
JohnParkerHodges <phodgesfl@gmail.com>
2019-10-27 22:29:05 -0400

    added all questions and button saves to localstorage

---
+++ assets/script/script.js
@@ -0,0 +1,67 @@
+let mainArea = document.getElementById("input-group");
+let userText = document.getElementById("#userText");
+let saveBtn = document.getElementById("#saveBtn");
+
+let textLocal = []
+    //function for saving value of row 9
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-9').val(localStorage.getItem("#time-9"));
+
+//function for saving value of row 10
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-10').val(localStorage.getItem("#time-10"));
+
+//function for saving value of row 11
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-11').val(localStorage.getItem("#time-11"));
+
+//function for saving value of row 12
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-12').val(localStorage.getItem("#time-12"));
+
+//function for saving value of row 1
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-1').val(localStorage.getItem("#time-1"));
+
+//function for saving value of row 2
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-2').val(localStorage.getItem("#time-2"));
+
+//function for saving value of row 3
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-3').val(localStorage.getItem("#time-3"));
+
+//function for saving value of row 4
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-4').val(localStorage.getItem("#time-4"));
+
+//function for saving value of row 5
+$('.sBtn').on("click", function() {
+    let temp = $(this.value).val()
+    localStorage.setItem(this.value, temp);
+})
+$('#time-5').val(localStorage.getItem("#time-5"));
